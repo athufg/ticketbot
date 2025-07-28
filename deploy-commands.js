49226dd3886d885f347b83685db399e92ffb706c
NIGHTMARE <81692563+athufg@users.noreply.github.com>
2025-07-28 14:36:34 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,21 +1,28 @@
-const { REST, Routes, SlashCommandBuilder } = require('discord.js');
-require('dotenv').config();
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+require("dotenv").config();
 
 const commands = [
-  new SlashCommandBuilder().setName('sendpanel').setDescription('Send ticket dropdown panel')
-].map(cmd => cmd.toJSON());
+  new SlashCommandBuilder()
+    .setName("sendpanel")
+    .setDescription("Sends the BUYING ticket panel"),
+  new SlashCommandBuilder()
+    .setName("sendpanel2")
+    .setDescription("Sends the SELLING ticket panel"),
+].map(command => command.toJSON());
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log('Registering slash commands...');
+    console.log("Started refreshing application (/) commands.");
+
     await rest.put(
-      Routes.applicationCommands('1391475230412898486'),
+      Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands }
     );
-    console.log('Commands registered.');
-  } catch (err) {
-    console.error(err);
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
   }
 })();
